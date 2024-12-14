5846c22cdec9035dfdbc73c3ef1bf1c74d06182e
Vinaydev007 <vinaybairavarapu@gmail.com>
2024-12-14 18:04:46 +0530

    updating

--- my-app/src/ui-content/home/main.js
+++ my-app/src/ui-content/home/main.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Updating from '../lifecycle/updatingphase/updating'
-import Functioncomp from '../functionalcomp/functioncomp'
+
 export default class Main extends Component {
   render() {
     return (
-      <div><Updating/></div>
-    // <Functioncomp/>
+      <div>
+        <Updating/>
+      </div>
     )
   }
 }
