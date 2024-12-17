765062f78c81d569862767676f437613499efc3b
Vinaydev007 <vinaybairavarapu@gmail.com>
2024-12-17 11:55:53 +0530

    Hooks concept

--- my-app/src/ui-content/home/main.js
+++ my-app/src/ui-content/home/main.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import Updating from '../lifecycle/updatingphase/updating'
+import Hooks from './functionalComp/hooks'
 
 export default class Main extends Component {
   render() {
     return (
-      <div><Updating/></div>
+      <div>
+        {/* <Updating/> */}
+        {<Hooks/>}
+      </div>
+
     )
   }
 }
